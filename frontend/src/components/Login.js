b8c998e77788feb94125f8fa4e9b9f38a3c5cbcb
Andriii Khymera <andriikhymera@MacBook-Pro-Andriii.local>
2021-03-26 00:30:52 +0200

    Rename files, fixed the post issue

---
+++ frontend/src/components/Login.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import {Card,Form,Button,Col, Row} from 'react-bootstrap'
+
+
+export default class Login extends Component {
+    render() {
+        return (
+            <Row className="justify-content-md-center">
+                <Col></Col>
+            </Row>
+        )
+    }
+}
